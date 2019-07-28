d9fe7ef660e08e7935d94beee6bf3fcfb3334e3c
Mikel Duke <mikelduke@gmail.com>
2019-07-28 12:55:39 -0500

    set version and title

--- conf.lua
+++ conf.lua
@@ -1,9 +1,7 @@
 -- Configuration
 function love.conf(t)
-    t.title = "Love Touch Effects"
-    -- t.version = "11.2"
-    -- t.window.width = 800
-    -- t.window.height = 800
+    t.title = "Magic Shapes"
+    t.version = "11.1"
     t.window.resizable = true
     t.window.fullscreen = true
     t.window.fullscreentype = exclusive
