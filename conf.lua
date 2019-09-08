5d27ab2dd0411cf959aa905a58531a3b65dd50f9
Mikel Duke <mikelduke@gmail.com>
2019-09-08 09:16:01 -0500

    set game version

--- conf.lua
+++ conf.lua
@@ -2,6 +2,8 @@
 function love.conf(t)
     t.title = "Magic Shapes"
     t.version = "11.1"
+    t.gameversion = "0.3"
+    t.window.title = "Magic Shapes"
     t.window.resizable = true
     t.window.fullscreen = true
     t.window.fullscreentype = exclusive
